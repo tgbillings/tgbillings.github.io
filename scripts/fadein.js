62d86ec91c555657364c541d97020fa2dc665fdb
Billings <teddy.billings@gmail.com>
2018-10-31 22:50:15 -0400

    adjusted the scroll fade in to 5th

--- scripts/fadein.js
+++ scripts/fadein.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
     
     $('.hideme').each( function(i){
-    var bottom_of_element = $(this).offset().top + $(this).outerHeight()/2;
+    var bottom_of_element = $(this).offset().top + $(this).outerHeight()/5;
     var bottom_of_window = $(window).scrollTop() + $(window).height();
     if( bottom_of_window > bottom_of_element ){
             $(this).animate({'opacity':'1'},500);
@@ -14,7 +14,7 @@ $(document).ready(function() {
         $('.hideme').each( function(i){
             
             
-            bottom_of_element = $(this).offset().top + $(this).outerHeight()/2;
+            bottom_of_element = $(this).offset().top + $(this).outerHeight()/5;
             bottom_of_window = $(window).scrollTop() + $(window).height();
             if( bottom_of_window > bottom_of_element ){
                 $(this).animate({'opacity':'1'},500);
